45607d411f15e84c26da276f44556f6c9c05f0be
“Gorgutzz” <leo.kalshteyn@gmail.com>
2022-05-21 22:40:02 -0700

    implemented jQuery and filtering by date and title for posts

--- js/main.js
+++ js/main.js
@@ -1,1 +1,42 @@
+// Filter Js
+$(document).ready(function () {
+  $(".filter-item").click(function () {
+    console.log("test");
+    const value = $(this).attr("data-filter");
+    let html = "";
+    if (value == "all") {
+      $(".post-box").show("1000");
 
+      html = $(".post-box").sort((a, b) => {
+        return $(a).attr("data-id") < $(b).attr("data-id") ? -1 : 1;
+      });
+    } else if (value == "sort") {
+      console.log("here");
+      html = $(".post-box").sort((a, b) => {
+        return $(a)
+          .find(".post-title")
+          .text()
+          .trim()
+          .localeCompare($(b).find(".post-title").text().trim());
+      });
+
+      // sort by date:
+    } else if (value == "sort-date") {
+      html = $(".post-box").sort((a, b) => {
+        const value =
+          new Date($(a).find(".post-date").text().trim()).getTime() <
+          new Date($(b).find(".post-date").text().trim()).getTime();
+
+        return !value ? -1 : 1;
+      });
+    }
+    $(".post.container").html(html);
+  });
+
+  $(".filter-item").click(function () {
+    $(this)
+      .addClass("active-filter")
+      .siblings()
+      .removeClass("active-filter");
+  });
+});
